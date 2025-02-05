17d62dddaf9d6110fdb8724693d8d3b26fe21cb9
SajitVentura <elopezv1700@alumno.ipn.mx>
2025-02-05 14:11:37 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./pages/Register/Register"; // Ruta correcta
+import Register from "./pages/Register/register"; // Ruta correcta
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/registro" element={<Register />} />
+        <Route path="/Register" element={<Register />} />
       </Routes>
     </Router>
   );
