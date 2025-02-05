6e4dd9a89a55c9cd0e6ec05985e9455ad68a59fd
SajitVentura <elopezv1700@alumno.ipn.mx>
2025-02-05 12:42:45 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-import './App.css'
+import Register from './pages/Register/register.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div></div>
+      <div>
+      <Route path="/registro" element={<Register />} />
+      </div>
     </>
   )
 }
